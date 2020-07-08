b2b80f24426459c8c37c8bc64bb2515a6db17ddd
sirmcclay <mcclay.design@gmail.com>
2020-07-07 21:38:10 -0300

    /transactions(POST)-service & repository-oing

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
@@ -16,7 +16,18 @@ transactionsRouter.get('/', async (request, response) => {
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
+
+  const createTransaction = new CreateTransactionService();
+
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,42 @@
+import { getCustomRepository } from 'typeorm';
+
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+// import Category from '../models/Category';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const findCategoryDuplicated = await transactionsRepository.findOne({
+      where: { category },
+    });
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: `${
+        findCategoryDuplicated ? findCategoryDuplicated.category_id : category
+      }`,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
